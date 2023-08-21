bd6b811231394bf6a762ce757977e0a5b64b0171
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2023-08-21 12:01:17 +0300

    feat: add skybox

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const BUILDER_URL = 'https://builder.decentraland.org'
 export const MIN_VALID_VP = 5
 export const SNAPSHOT_SPACE = 'snapshot.dcl.eth'
 
-export const PARCEL_COUNT = 2 // 2 rows, 2 columns
+export const PARCEL_COUNT = 4 // 4 rows, 4 columns
 export const PARCEL_SIZE = 16
 export const SCENE_SIZE = PARCEL_COUNT * PARCEL_SIZE
 export const SCENE_MIDDLE = SCENE_SIZE / 2
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { Vector3 } from '@dcl/sdk/math'
 import { SCENE_MIDDLE } from './config'
 import { createTopVoterBoard, createVotingBoard } from './board'
 import { createLaptops } from './laptops'
+import { createSkyBox } from './skyBox'
 
 export function main() {
   const scene = engine.addEntity(false)
   Transform.create(scene, { position: Vector3.create(SCENE_MIDDLE, 0, SCENE_MIDDLE) })
   GltfContainer.create(scene, { src: 'models/scene.glb' })
+
+  const border = engine.addEntity(false)
+  Transform.create(border, { parent: scene })
+  GltfContainer.create(border, { src: 'models/border.glb' })
 
   createLaptops(scene)
 
@@ -18,4 +23,6 @@ export function main() {
   executeTask(async () => {
     await createTopVoterBoard(scene)
   })
+
+  createSkyBox(scene)
 }
